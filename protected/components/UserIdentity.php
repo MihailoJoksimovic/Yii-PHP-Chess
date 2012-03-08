5c84dae55e8cfda64edd3a3f33616a13ad8088d5
Mihailo Joksimovic <tinzey@gmail.com>
2012-03-08 02:47:46 +0100

    User model and updated UserIdentity to handle login from DB

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,21 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
+		$model = User::model()->findByAttributes(array('email' => $this->username));
+		
+		if ( ! $model)
+		{
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
+		}
+		elseif($model->password != $this->password)
+		{
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}
 		else
+		{
 			$this->errorCode=self::ERROR_NONE;
+		}
+	
 		return !$this->errorCode;
 	}
 }
