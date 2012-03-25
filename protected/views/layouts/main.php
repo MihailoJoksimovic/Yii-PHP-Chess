8deb5d10aee8486069c4be6d0cbcb3f8302eadd4
Mihailo Joksimovic <tinzey@gmail.com>
2012-03-25 22:06:29 +0200

    Revert "Cosmetics"
    This reverts commit 3913cd79dadcf2a8bb073eed62a078366d0aeea6.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -24,7 +24,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><span style="color:gold">I</span>ncredible<span style="color:gold">C</span>hess<span style="color:gold">T</span>ournament</div>
+		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
@@ -49,7 +49,8 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		<p>Developed by Mihailo Joksimović (56/08)</p>
+		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,98 +1,16 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <span style="color:gold">I</span>ncredible<span style="color:gold">C</span>hess<span style="color:gold">T</span>ournament</h1>
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<table border="0">
-	<tr>
-		<td style="width: 450px; vertical-align: top;" valign="top">
-			<h2>Login</h2>
+<p>Congratulations! You have successfully created your Yii application.</p>
 
-			<p>Please fill out the following form with your login credentials:</p>
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
+	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
+</ul>
 
-			<div class="form">
-			<?php $loginForm=$this->beginWidget('CActiveForm', array(
-				'id'=>'login-form',
-				'enableClientValidation'=>true,
-				'clientOptions'=>array(
-					'validateOnSubmit'=>true,
-				),
-				'action' => array('site/login'),
-			)); $loginModel = new LoginForm; ?>
-
-				<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-				<div class="row">
-					<?php echo $loginForm->labelEx($loginModel,'username'); ?>
-					<?php echo $loginForm->textField($loginModel,'username'); ?>
-					<?php echo $loginForm->error($loginModel,'username'); ?>
-				</div>
-
-				<div class="row">
-					<?php echo $loginForm->labelEx($loginModel,'password'); ?>
-					<?php echo $loginForm->passwordField($loginModel,'password'); ?>
-					<?php echo $loginForm->error($loginModel,'password'); ?>
-				</div>
-
-				<div class="row rememberMe">
-					<?php echo $loginForm->checkBox($loginModel,'rememberMe'); ?>
-					<?php echo $loginForm->label($loginModel,'rememberMe'); ?>
-					<?php echo $loginForm->error($loginModel,'rememberMe'); ?>
-				</div>
-
-				<div class="row buttons">
-					<?php echo CHtml::submitButton('Login'); ?>
-				</div>
-
-			<?php $this->endWidget(); ?>
-			</div><!-- form -->
-		</td>
-		<td valign="top">
-			<h2>Register</h2>
-			
-			<div class="form">
-
-				<?php $form=$this->beginWidget('CActiveForm', array(
-					'id'=>'user-register-form',
-					'enableAjaxValidation'=>false,
-					'action' => array('site/register'),
-				)); /* @var $form CActiveForm */ $model = new User; ?>
-
-					<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-					<?php echo $form->errorSummary($model); ?>
-
-					<div class="row">
-						<?php echo $form->labelEx($model,'name'); ?>
-						<?php echo $form->textField($model,'name'); ?>
-						<?php echo $form->error($model,'name'); ?>
-					</div>
-
-					<div class="row">
-						<?php echo $form->labelEx($model,'email'); ?>
-						<?php echo $form->textField($model,'email'); ?>
-						<?php echo $form->error($model,'email'); ?>
-					</div>
-
-					<div class="row">
-						<?php echo $form->labelEx($model,'password'); ?>
-						<?php echo $form->passwordField($model,'password'); ?>
-						<?php echo $form->error($model,'password'); ?>
-					</div>
-
-					<div class="row">
-						<?php echo $form->labelEx($model,'repeat_password'); ?>
-						<?php echo $form->passwordField($model,'repeat_password'); ?>
-						<?php echo $form->error($model,'repeat_password'); ?>
-					</div>
-
-
-					<div class="row buttons">
-						<?php echo CHtml::submitButton('Submit'); ?>
-					</div>
-
-				<?php $this->endWidget(); ?>
-
-				</div><!-- form -->
-		</td>
-	</tr>
-</table>
+<p>For more details on how to further develop this application, please read
+the <a href="http://www.yiiframework.com/doc/">documentation</a>.
+Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
+should you have any questions.</p>
